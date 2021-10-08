7fba1cc00b74432e2b720039ddd87a7cab6e38c7
toso0527 <87591651+toso0527@users.noreply.github.com>
2021-10-09 06:56:19 +0900

    Delete web.php

--- web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthorController;
-
-Route::get('/', [AuthorController::class, 'thanks']);
-Route::get('/index', [AuthorController::class, 'index']);
-Route::post('/index', [AuthorController::class, 'create']);
